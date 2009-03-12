d16e4fbe26db26dd99510ea6f54e2030b95eee16
Clinton Ecker <clint@arstechnica.com>
2009-03-12 17:39:35 -0500

    Cleaning up output and adding more examples to readme

--- README.md
+++ README.md
@@ -3,8 +3,19 @@ RSS Feeds for irccat
 
 Start irccat somewhere, and invoke this script as follows:
 
-./irccat-rss.py FEED_URL CHANNEL_LIST [PREFIX] &
+./irccat-rss.py --feed FEED_URL [--channels CHANNEL_LIST] [--prefix PREFIX] [--host HOST] [--port PORT] [--netcat-binary NCBIN] [--netcat-options NCOPTS] &
 
-### Example ###
+* CHANNEL_LIST defaults to "#*" or all connected channels
+* PREFIX defaults to blank
+* HOST defaults to 127.0.0.1
+* POST defaults to 12345
+* NCBIN defaults to /bin/netcat -- Note on OS X you should specify --netcat-binary /usr/bin/nc
+* NCOPTS defaults to blank. On Linux style system you may want to specify "-q 1" or "-q0"
 
-./irccat-rss.py http://search.twitter.com/search.atom?q=apple+event \#appleevent Mar\ 17\ Event &
+### Examples ###
+
+./irccat-rss.py --feed http://search.twitter.com/search.atom?q=apple+event --channels \#appleevent --prefix "Mar 17 Event" &
+./irccat-rss.py --feed http://search.twitter.com/search.atom?q=apple+event --channels \#appleevent --prefix "Mar 17 Event" &
+./irccat-rss.py --feed http://arstechnica.com/rss/everything.xml --channels \#mychan --prefix "Everything Feed" -n /usr/bin/nc --port 12346 &
+./irccat-rss.py --feed http://arstechnica.com/rss/everything.xml --channels \#mychan,\#yourchan2,@joeschmoe
+./irccat-rss.py --feed http://arstechnica.com/rss/everything.xml --channels \#mychan --prefix "Everything Feed" -n /usr/bin/nc -o "-q0" --port 12346 &
--- irccat-rss.py
+++ irccat-rss.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 import feedparser, time, sys, os, urllib
+from optparse import OptionParser
 
-def printout(s, channels):
+def printout(s, channels, host, port, ncbin, ncopts):
   s = s.replace('"', '\"')
   try:
-    o = os.popen('/bin/echo "%s %s" | /bin/netcat -q 1 127.0.0.1 12345' % (channels,s)).read()
+    out = '/bin/echo "%s %s" | %s %s %s %s' % (channels,s, ncbin, ncopts, host, port)
+    o = os.popen(out).read()
   except:
     return
     
 def tinyurl(long_url):
   return urllib.urlopen('http://tinyurl.com/api-create.php?url=%s' % long_url).read()
   
-def main(feed, name, channels="*#"):
-  printout("Restarted, piping from %s to %s" % (feed, channels), channels)
+def main(feed, channels, prefix, host, port, ncbin, ncopts):
+  printout("Restarted, piping from %s to %s" % (feed, channels), channels, host, port, ncbin, ncopts)
   e = {}
   c = 0
   d = feedparser.parse(feed)
@@ -20,21 +22,33 @@ def main(feed, name, channels="*#"):
     if entry.guid not in e.keys():
       c += 1
       e[entry.guid] = entry
-  printout('preloaded %s items' % (c,), channels)
+  printout('preloaded %s items' % (c,), channels, host, port, ncbin, ncopts)
   while 1:
     time.sleep(10)
     d = feedparser.parse(feed)
     for entry in d['entries']:
       if entry.guid not in e.keys():
         e[entry.guid] = entry
-        printout("%s: %s - %s" % (name, entry.title, tinyurl(entry.link)), channels)
+        printout("%s: %s - %s" % (prefix, entry.title, tinyurl(entry.link)), channels, host, port, ncbin, ncopts)
       
 if __name__ == "__main__":
-  if len(sys.argv) < 3:
-    print "Please provide a feed and a channel list"
-    sys.exit(-5000)
-  if len(sys.argv) == 4:
-    name = sys.argv[3] + ": "
-  else:
-    name = ""
-  main(feed=sys.argv[1],channels=sys.argv[2],name=name)
+  parser = OptionParser()
+  parser.add_option("-f", "--feed", dest="feed",
+                    help="The URL of the RSS/Atom/XML Feed")
+  parser.add_option("-c", "--channels",
+                    dest="channels", default="#*", help="A channel or list of channels: #mychan or #mychan1,#mychan2,@joeblow")
+  parser.add_option("-p", "--prefix", default="",
+                    dest="prefix", help="A descriptive string to print before the entry title")
+  parser.add_option("-H", "--host",
+                    dest="host", default="127.0.0.1", help="The hostname or IP address of the server running irccat bot")
+  parser.add_option("-P", "--port",
+                    dest="post", default="12345", help="The port that the irccat bot is running on")
+  parser.add_option("-n", "--netcat-binary",
+                    dest="ncbin", default="/bin/netcat", help="Location and name of netcat binary")
+  parser.add_option("-o", "--netcat-options",
+                    dest="ncopts", default="", help="Additional options to send to netcat")
+  
+
+  (options, args) = parser.parse_args()
+  
+  main(feed=options.feed,channels=options.channels,prefix=options.prefix,host=options.host,port=options.post,ncbin=options.ncbin,ncopts=options.ncopts)
