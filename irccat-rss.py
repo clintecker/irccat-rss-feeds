25c10e1a277a3ddcf326e83d1a43a9716917020a
Clinton Ecker <clint@arstechnica.com>
2009-03-12 22:56:41 -0500

    Adding re module, duh

--- irccat-rss.py
+++ irccat-rss.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import feedparser, time, sys, os, urllib
+import feedparser, time, sys, os, urllib, re
 from optparse import OptionParser
 
 def printout(s, channels, host, port, ncbin, ncopts):
